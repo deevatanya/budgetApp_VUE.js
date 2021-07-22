2238617d145c8b3cb1a073694cd3a9d6954d7ad1
deevatanya <deewa.tatjana@yandex.ru>
2021-07-22 23:23:36 +0300

    first commit v.1

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,28 @@
 module.exports = {
   root: true,
+
   env: {
     node: true,
   },
+
   extends: [
     'plugin:vue/essential',
     '@vue/airbnb',
   ],
+
   parserOptions: {
     parser: 'babel-eslint',
   },
+
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   },
+
+  extends: [
+    "eslint:recommended",
+    'plugin:vue/strongly-recommended',
+    '@vue/airbnb',
+    'plugin:vue/essential',
+  ],
 };
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
 import App from './App.vue';
+import './plugins/elements';
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: (h) => h(App),
+/* eslint-disable-next-line */
+  render: h => h(App)
 }).$mount('#app');
